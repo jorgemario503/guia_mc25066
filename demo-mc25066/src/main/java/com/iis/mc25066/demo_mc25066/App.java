9c79d82420c681fa3d96d53fa8842bc22212f761
jorge <jorgemarmel@gmail.com>
2025-05-11 20:58:50 -0600

    agregando maven

---
+++ demo-mc25066/src/main/java/com/iis/mc25066/demo_mc25066/App.java
@@ -0,0 +1,16 @@
+package com.iis.mc25066.demo_mc25066;
+
+/**
+ * Hello world!
+ */
+public class App {
+	
+	public String Hello() {
+		return "Hello World";
+	}
+	
+    public static void main(String[] args) {
+    	App hw = new App();
+        System.out.println(hw.Hello());
+    }
+}
---
+++ demo-mc25066/src/test/java/com/iis/mc25066/demo_mc25066/AppTest.java
@@ -0,0 +1,19 @@
+package com.iis.mc25066.demo_mc25066;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testHello() {
+        assertTrue(true);
+    }
+}
